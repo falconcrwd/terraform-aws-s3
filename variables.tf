776b90f6d9270b4dc8530bfeab4af2b7675edf11
chianw <chiancw@gmail.com>
2026-09-12 10:42:23 +0800

    testpr1

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "sg_name" {
   description = "Name of the security group"
   type        = string
-  default     = "test1-nsg"
+  default     = "test2-nsg"
 }
 
 variable "allowed_ingress_cidrs" {
